d214c1edf1576815ebf724eae6c810ae7adf0d0d
Tatyana Pushchaeva <pushchaeva2000@mail.ru>
2024-12-15 22:12:36 +0500

    HomeWork-3-SauceDemo

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,6 +3,7 @@ package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.asserts.SoftAssert;
 import pages.LoginPage;
